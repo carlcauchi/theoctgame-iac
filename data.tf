533a253aa6af0ea4b807e015343ac86ad15c171d
Carl Cauchi <carl.cauchi@keyrock.eu>
2024-05-21 15:48:23 +0200

    updated token

--- data.tf
+++ data.tf
@@ -1,2 +1,10 @@
 data "aws_caller_identity" "current" {} # data.aws_caller_identity.current.account_id
 data "aws_region" "current" {}          # data.aws_region.current.name
+
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
--- provider.tf
+++ provider.tf
@@ -14,15 +14,15 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = module.eks.token
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = module.eks.token
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.default.token
   }
 }
